ac8b664cc0dd057c1cc5d6a08d1e86018c1d1485
Alastair Tse <alastair@liquidx.net>
2022-12-05 19:31:46 +0900

    Made filtering by URL work.

--- src/lib/mem-data-queries.ts
+++ src/lib/mem-data-queries.ts
@@ -1,5 +1,5 @@
 import { unwrapDocs } from '@/firebase'
-import { CollectionReference, DocumentData, doc, getDoc, getDocs, query, where, orderBy, limit, WhereFilterOp } from 'firebase/firestore'
+import { CollectionReference, DocumentData, QueryConstraint, Query, getDocs, query, where, orderBy, limit, WhereFilterOp } from 'firebase/firestore'
 
 export function queryForAllMems(collection: CollectionReference<DocumentData>): Promise<Array<DocumentData>> {
   return getDocs(collection).then(unwrapDocs)
@@ -19,13 +19,20 @@ export function queryForTaggedMems(collection: CollectionReference<DocumentData>
   tags: Array<string>,
   matchOperator: string,
   pageSize: number): Promise<Array<DocumentData>> {
-  let whereOperator: WhereFilterOp = 'array-contains-any'
-  switch (matchOperator) {
-    case 'any':
-      whereOperator = 'array-contains-any'
-    case 'all':
-      whereOperator = 'array-contains'
+
+  if (matchOperator === 'all') {
+    // Limitation of Firestore is that we can't combined multple array-contains conditions.
+    // So we have to take the first tag and query for that, then filter the results.
+    const firstTag = tags[0]
+    const q = query(collection, where('tags', 'array-contains', firstTag), orderBy('addedMs', 'desc'), limit(pageSize))
+    return getDocs(q).then(unwrapDocs).then(mems => {
+      return mems.filter(mem => {
+        return tags.every(tag => mem.tags.includes(tag))
+      })
+    })
+
+  } else {
+    const q = query(collection, where('tags', 'array-contains-any', tags), orderBy('addedMs', 'desc'), limit(pageSize))
+    return getDocs(q).then(unwrapDocs)
   }
-  const q = query(collection, where('tags', whereOperator, tags), orderBy('addedMs', 'desc'), limit(pageSize))
-  return getDocs(q).then(unwrapDocs)
 }
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,12 +2,23 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Home from "../views/Home.vue";
 import DataPage from "../views/DataPage.vue";
 import About from "../views/About.vue";
+import Single from '../views/Single.vue';
 
 const routes = [
   {
     path: "/",
     name: "Home",
     component: Home
+  },
+  {
+    path: "/tag/:tags",
+    name: "TagList",
+    component: Home
+  },
+  {
+    path: "/mem/:id",
+    name: "SingleMem",
+    component: Single
   },
   {
     path: "/about",
