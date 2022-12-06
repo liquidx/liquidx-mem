a681158ea2cff550b7d7da8339e75bb1d8de2730
Alastair Tse <alastair@liquidx.net>
2022-12-06 17:29:38 +0900

    Fix issues with add not working.

--- functions/core/parser.ts
+++ functions/core/parser.ts
@@ -1,6 +1,5 @@
 import { Mem } from "./mems";
-//import * as urlRegex from "url-regex-safe";
-import urlRegexSafe from 'url-regex-safe'
+import urlRegexSafe from "url-regex-safe";
 
 const tagRegex = new RegExp("#\\w+", "g");
 const dateRegex = new RegExp("[0-9]{4}-[0-9]{2}-[0-9]{2}");
