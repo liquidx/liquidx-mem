cfba8de811c454b912154b8fd07e91778561f10f
Alastair Tse <alastair@liquidx.net>
2024-07-20 14:47:24 +0900

    Add twitter URLs as non opengraph-able

--- src/lib/server/annotator-og-blocklist.ts
+++ src/lib/server/annotator-og-blocklist.ts
@@ -1,21 +1,25 @@
-export const ogBlockList: Record<string, Array<string>> = {
-  ogUrl: ["https://www.instagram.com/accounts/login/"]
+export const ogBlockList: { [key: string]: RegExp[] } = {
+	ogUrl: [new RegExp('https://www.instagram.com/accounts/login/')],
+	ogTitle: [new RegExp('^x.com$')]
 };
 
 export const isResultBlocked = (result: any): boolean => {
-  if (!result) {
-    return false;
-  }
-  for (const param of Object.keys(ogBlockList)) {
-    if (!result[param]) {
-      continue;
-    }
+	if (!result) {
+		return false;
+	}
+	for (const param of Object.keys(ogBlockList)) {
+		if (!result[param]) {
+			continue;
+		}
 
-    const invalidValues = ogBlockList[param];
-    const val = result[param] as string;
-    if (invalidValues.includes(val)) {
-      return true;
-    }
-  }
-  return false;
+		const invalidValues = ogBlockList[param];
+		const val = result[param] as string;
+		for (const invalidValue of invalidValues) {
+			//
+			if (val.match(invalidValue)) {
+				return true;
+			}
+		}
+	}
+	return false;
 };
