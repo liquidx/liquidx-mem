198601edc87139d1747edaf8d96fd0725ee9dcc2
Alastair Tse <alastair@liquidx.net>
2024-07-20 16:06:50 +0900

    Update dependencies and fetch

--- tests/manual-annotate-test.js
+++ tests/manual-annotate-test.js
@@ -1,17 +1,13 @@
 // End to end tests for Firebase Functions.
-const fetch = require("make-fetch-happen");
+const axios = require('axios');
 
-const server = "http://localhost:5001/liquidx-mem/us-central1";
+const server = 'http://localhost:5001/liquidx-mem/us-central1';
 //const server = "https://liquidx-mem.web.app/api";
 
 const testAnnotate = (userId, memId) => {
-  fetch(`${server}/annotate?user=${userId}&mem=${memId}`, {
-    method: "GET"
-  })
-    .then(response => response.text())
-    .then(response => {
-      console.log(response);
-    });
+	axios.get(`${server}/annotate?user=${userId}&mem=${memId}`).then((response) => {
+		console.log(response.data);
+	});
 };
 
-testAnnotate("BB8zGVrCbrQ2QryHyiZNaUZJjQ93", "57xoLmtby1lGIy7QgnQx");
+testAnnotate('BB8zGVrCbrQ2QryHyiZNaUZJjQ93', '57xoLmtby1lGIy7QgnQx');
