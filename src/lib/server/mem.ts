f8d333e8f2a7ee3f4b7c8b68426d3218b1497894
Alastair Tse <alastair@liquidx.net>
2024-07-21 12:21:43 +0900

    Create groups of routing so we can output non HTML output.

--- src/lib/server/mem.ts
+++ src/lib/server/mem.ts
@@ -11,12 +11,31 @@ export const getMem = (db: Firestore, userId: string, memId: string): Promise<Do
 	return db.collection('users').doc(userId).collection('mems').doc(memId).get();
 };
 
-export const getAllMems = async (db: Firestore, userId: string): Promise<Mem[]> => {
-	const ref: CollectionReference<DocumentData> | Query<DocumentData> = db.collection(
+export interface MemOptions {
+	maxResults?: number;
+	lookQueue?: boolean;
+}
+
+export const getAllMems = async (
+	db: Firestore,
+	userId: string,
+	queryOptions?: MemOptions
+): Promise<Mem[]> => {
+	let query: CollectionReference<DocumentData> | Query<DocumentData> = db.collection(
 		`users/${userId}/mems`
 	);
 
-	const collection = await ref.orderBy('addedMs', 'desc').get();
+	query = query.orderBy('addedMs', 'desc');
+	if (queryOptions) {
+		if (queryOptions.lookQueue) {
+			query = query.where('tags', 'array-contains', '#look');
+		}
+		if (queryOptions.maxResults) {
+			query = query.limit(queryOptions.maxResults);
+		}
+	}
+
+	const collection = await query.get();
 
 	const mems: Mem[] = [];
 	collection.forEach((doc: DocumentSnapshot<DocumentData>) => {
--- src/routes/m/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import MainView from '$lib/svelte/MainView.svelte';
-</script>
-
-<svelte:head>
-	<title>#mem</title>
-</svelte:head>
-
-<MainView filter="" showTags={false} />
