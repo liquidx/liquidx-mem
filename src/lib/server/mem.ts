9a8368633e4406d1ade296e8b282d58f20c29540
Alastair Tse <alastair@liquidx.net>
2024-07-21 11:49:29 +0900

    Add some helper methods for getting mems

--- src/lib/server/mem.ts
+++ src/lib/server/mem.ts
@@ -1,6 +1,28 @@
 import type { Firestore } from '@google-cloud/firestore';
-import type { DocumentSnapshot } from 'firebase-admin/firestore';
+import type {
+	DocumentSnapshot,
+	DocumentData,
+	CollectionReference,
+	Query
+} from 'firebase-admin/firestore';
+import type { Mem } from '$lib/common/mems';
 
 export const getMem = (db: Firestore, userId: string, memId: string): Promise<DocumentSnapshot> => {
 	return db.collection('users').doc(userId).collection('mems').doc(memId).get();
+};
+
+export const getAllMems = async (db: Firestore, userId: string): Promise<Mem[]> => {
+	const ref: CollectionReference<DocumentData> | Query<DocumentData> = db.collection(
+		`users/${userId}/mems`
+	);
+
+	const collection = await ref.orderBy('addedMs', 'desc').get();
+
+	const mems: Mem[] = [];
+	collection.forEach((doc: DocumentSnapshot<DocumentData>) => {
+		const mem = Object.assign({}, doc.data(), { id: doc.id });
+		mems.push(mem);
+	});
+
+	return mems;
 };
