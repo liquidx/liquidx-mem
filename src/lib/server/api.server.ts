61ff375578ab6471cb287b834c16debbdf1f6451
Alastair Tse <alastair@liquidx.net>
2024-07-21 11:49:29 +0900

    Add some helper methods for getting mems

--- src/lib/server/api.server.ts
+++ src/lib/server/api.server.ts
@@ -1,3 +1,4 @@
+import type { UserRecord } from 'firebase-admin/auth';
 import { getAuth } from 'firebase-admin/auth';
 import { type App } from 'firebase-admin/app';
 
@@ -28,4 +29,32 @@ export const getUserId = async (
 	}
 
 	return userId;
+};
+
+export const getUser = async (
+	firebaseApp: App,
+	request: Request
+): Promise<UserRecord | undefined> => {
+	// Check auth
+	const token = request.headers.get('Authorization')?.replace('Bearer ', '');
+	if (!token) {
+		console.log('Error: No token');
+		return;
+	}
+
+	const decodedToken = await getAuth(firebaseApp).verifyIdToken(token);
+	if (!decodedToken) {
+		return;
+	}
+
+	const userId = decodedToken.uid;
+	if (!userId || userId === USER_NOT_FOUND) {
+		return;
+	}
+
+	const user = await getAuth(firebaseApp).getUser(userId);
+	if (user) {
+		return user;
+	}
+	return;
 };
